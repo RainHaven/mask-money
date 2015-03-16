b133f637dd409b85302cf9bcab03452bc7115a82
Scott McPherson <scott_fl@me.com>
2015-03-16 14:23:21 -0400

    update summary and git url

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'rainhaven:mask-money',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'jQuery Mask Money for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/RainHaven/mask-money',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
